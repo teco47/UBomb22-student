dbdf086c8d2899ef09c87e7f01e867b01a0a86d3
tpaolini <timothe.paolini@etu.u-bordeaux.fr>
2022-11-11 12:35:52 +0100

    Add the start of World.java

---
+++ src/main/java/fr/ubx/poo/ubomb/launcher/World.java
@@ -0,0 +1,16 @@
+package fr.ubx.poo.ubomb.launcher;
+
+import fr.ubx.poo.ubomb.game.Configuration;
+
+import java.util.List;
+
+public class World {
+
+    private final int nbLevel;
+    private final List<String> levels;
+    private int playerLevel;
+
+    private Configuration config;
+
+    public
+}
